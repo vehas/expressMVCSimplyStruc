b5a4bbef4ba285bada8d9958bb1f54ca76bff359
vehas <sky.4748@gmail.com>
2015-10-28 12:47:53 +0700

    first running  server

---
+++ app.es6.js
@@ -0,0 +1,21 @@
+/* eslint-env:node */
+let express = require('express'),
+  bodyParser = require('body-parser');
+let app = express(),
+  port = 3000;
+
+// setup
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// smaple
+app.get('/', (req, res, next) => {
+  res.end('hi nodejs');
+});
+
+// run
+app.listen(port, () => {
+  console.log(`Listening on port  :${port}`);
+});
